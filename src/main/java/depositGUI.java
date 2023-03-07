77363c10a61693e979d18c280a87fd3165e4a2f0
Josh <jnievelstein@yahoo.com>
2023-03-07 14:47:35 +0100

    fixed DEPO problems

--- src/main/java/depositGUI.java
+++ src/main/java/depositGUI.java
@@ -4,9 +4,8 @@ import java.awt.*;
 import java.awt.datatransfer.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.io.*;
+import java.util.ArrayList;
 
 public class depositGUI extends JFrame implements ActionListener {
     public JPanel depositPanel;
@@ -35,24 +34,44 @@ public class depositGUI extends JFrame implements ActionListener {
                 return column != 0; // allow editing only in columns 2-4
             }
         };
-        String[] columns = {"#","Content"};
-        depositTableModel = new DefaultTableModel(columns, 0);
-        // add "new row" entry if there's no content in the table
-        if (depositTableModel.getRowCount() == 0) {
-            Object[] newRow = {"1", ""};
-            depositTableModel.addRow(newRow);
-            rowCount++;
+        String[] columns = {"#", "Content"};
+        // Step 2: Check if the log.txt file exists
+        if (!logFile.exists()) {
+            // If it doesn't, create an empty depositTableModel
+            depositTableModel = new DefaultTableModel(columns, 0);
+            if (depositTableModel.getRowCount() == 0) {
+                Object[] newRow = {"1", ""};
+                depositTableModel.addRow(newRow);
+                rowCount++;
+            }
+        } else {
+            try {
+                // Step 3: Read the data from the log.txt file and create a new depositTableModel
+                BufferedReader reader = new BufferedReader(new FileReader(logFile));
+                String line;
+                ArrayList<String[]> dataList = new ArrayList<>();
+                while ((line = reader.readLine()) != null) {
+                    String[] rowData = line.split(",");
+                    dataList.add(rowData);
+                }
+                reader.close();
+                depositTableModel = new DefaultTableModel(dataList.toArray(new Object[0][0]), columns);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-
+        // Step 4: Set the depositTableModel as the model of the JTable
         depositTable = new JTable(depositTableModel);
+        depositTable.setModel(depositTableModel);
+
         scrollPane = new JScrollPane(depositTable);
         depositPanel.add(scrollPane, BorderLayout.CENTER);
-
+        // buttons
         addButton = new JButton("Add");
         saveButton = new JButton("Save");
         deleteButton = new JButton("Delete");
         copyButton = new JButton("Copy");
-
+        // add section (add things to panels)
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
         buttonPanel.add(addButton);
@@ -73,17 +92,25 @@ public class depositGUI extends JFrame implements ActionListener {
         setLocationRelativeTo(null);
         setVisible(true);
     }
-
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == addButton) {
-            //depositTableModel.addRow(new Object[]{"", "", "", ""});
-            Object[] newRow = {Integer.toString(rowCount), ""};
-            depositTableModel.addRow(newRow);
-            rowCount++;
-        }if (e.getSource() == addButton) {
-            Object[] newRow = {Integer.toString(getNextNumber()), ""};
-            depositTableModel.addRow(newRow);
+            int selectedRow = depositTable.getSelectedRow();
+            // add a new row at the end of the table if no row is selected
+            if (selectedRow == -1) {
+                Object[] newRow = {Integer.toString(depositTableModel.getRowCount() + 1), ""};
+                depositTableModel.addRow(newRow);
+                rowCount++;
+            } else {
+                // update the row numbers
+                for (int i = selectedRow; i < depositTableModel.getRowCount(); i++) {
+                    depositTableModel.setValueAt(i + 1, i, 0);
+                }
+                // insert a new row below the selected row
+                Object[] newRow = {Integer.toString(selectedRow + 2), ""};
+                depositTableModel.insertRow(selectedRow + 1, newRow);
+                rowCount++;
+            }
         } else if (e.getSource() == saveButton) {
             // save the content of the selected row
             try {
@@ -142,7 +169,7 @@ public class depositGUI extends JFrame implements ActionListener {
     private int getNextNumber() {
         int nextNumber = 1;
         for (int i = 0; i < depositTableModel.getRowCount(); i++) {
-            int currentNumber = Integer.parseInt((String) depositTableModel.getValueAt(i, 0));
+            int currentNumber = Integer.parseInt(depositTableModel.getValueAt(i, 0).toString());
             if (currentNumber >= nextNumber) {
                 nextNumber = currentNumber + 1;
             }
