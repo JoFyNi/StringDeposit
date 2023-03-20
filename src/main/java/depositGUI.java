20d473a277e118dea01b17574d5c9685e708dc47
Josh <jnievelstein@yahoo.com>
2023-03-20 15:17:49 +0100

    added PopupMenu and a open funktion for the links

--- src/main/java/depositGUI.java
+++ src/main/java/depositGUI.java
@@ -4,7 +4,10 @@ import java.awt.*;
 import java.awt.datatransfer.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.*;
+import java.net.URI;
 import java.util.ArrayList;
 
 public class depositGUI extends JFrame implements ActionListener {
@@ -16,16 +19,17 @@ public class depositGUI extends JFrame implements ActionListener {
     public JButton saveButton;
     public JButton deleteButton;
     public JButton copyButton;
+    public JButton openButton;
     public File logFile = new File("log.txt");
     static Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
     private int rowCount = 1;
     String version = System.getProperty("java.version");
     public depositGUI() {
         setTitle("DEPO      Version: " + version);
-        setSize(440, 400);
+        setSize(640, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         depositPanel = new JPanel();
-        depositPanel.setPreferredSize(new Dimension(440, 400));
+        depositPanel.setPreferredSize(new Dimension(640, 400));
         depositPanel.setLayout(new BorderLayout());
         getContentPane().add(depositPanel, BorderLayout.CENTER);
         depositTableModel = new DefaultTableModel() {
@@ -34,13 +38,13 @@ public class depositGUI extends JFrame implements ActionListener {
                 return column != 0; // allow editing only in columns 2
             }
         };
-        String[] columns = {"#", "Content"};
+        String[] columns = {"#", "Name", "Link"};
         // Step 2: Check if the log.txt file exists
         if (!logFile.exists()) {
             // If it doesn't, create an empty depositTableModel
             depositTableModel = new DefaultTableModel(columns, 0);
             if (depositTableModel.getRowCount() == 0) {
-                Object[] newRow = {"1", ""};
+                Object[] newRow = {"1", "", ""};
                 depositTableModel.addRow(newRow);
                 rowCount++;
             }
@@ -69,13 +73,16 @@ public class depositGUI extends JFrame implements ActionListener {
         depositTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         depositTable.getColumnModel().getColumn(0).setPreferredWidth(20);
         depositTable.getColumnModel().getColumn(0).setWidth(20);
-        depositTable.getColumnModel().getColumn(1).setWidth(200);
-        depositTable.getColumnModel().getColumn(1).setPreferredWidth(400);
+        depositTable.getColumnModel().getColumn(1).setWidth(150);
+        depositTable.getColumnModel().getColumn(1).setPreferredWidth(300);
+        depositTable.getColumnModel().getColumn(2).setWidth(150);
+        depositTable.getColumnModel().getColumn(2).setPreferredWidth(300);
         // buttons
         addButton = new JButton("Add");
         saveButton = new JButton("Save");
         deleteButton = new JButton("Delete");
         copyButton = new JButton("Copy");
+        openButton = new JButton("Open");
         // add section (add things to panels)
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
@@ -86,16 +93,20 @@ public class depositGUI extends JFrame implements ActionListener {
         buttonPanel.add(deleteButton);
         buttonPanel.add(Box.createHorizontalGlue());
         buttonPanel.add(copyButton);
+        buttonPanel.add(Box.createHorizontalGlue());
+        buttonPanel.add(openButton);
         depositPanel.add(buttonPanel, BorderLayout.SOUTH);
 
         addButton.addActionListener(this);
         saveButton.addActionListener(this);
         deleteButton.addActionListener(this);
         copyButton.addActionListener(this);
+        openButton.addActionListener(this);
         // designe for the frame
         getContentPane().setFont(new Font("Times New Roman", Font.PLAIN, 20));
         setLocationRelativeTo(null);
         setVisible(true);
+        buttons();
     }
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -150,10 +161,65 @@ public class depositGUI extends JFrame implements ActionListener {
             // copy the content of the selected row
             int selectedRow = depositTable.getSelectedRow();
             if (selectedRow != -1) {
-                String content = (String) depositTableModel.getValueAt(selectedRow, 1);
+                String content = (String) depositTableModel.getValueAt(selectedRow, 2);
                 copyToClipboard(content);
             }
+        } else if (e.getSource() == openButton) {
+            // copy the content of the selected row
+            int selectedRow = depositTable.getSelectedRow();
+            if (selectedRow != -1) {
+                String selectedURL = (String) depositTable.getValueAt(selectedRow, 2);
+                Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+                if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+                    try {
+                        System.out.println(selectedURL);
+                        URI uri = new URI(selectedURL);
+                        desktop.browse(uri);
+                    } catch (Exception browserException) {
+                        browserException.printStackTrace();
+                    }
+                }
+            }
         }
+    }
+    void buttons() {
+        depositTable.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (SwingUtilities.isRightMouseButton(e)) {
+                    // create and show the JPopupMenu
+                    JPopupMenu menu = new JPopupMenu();
+                    JMenuItem copyItem = new JMenuItem("Copy");
+                    JMenuItem openItem = new JMenuItem("Open");
+
+                    int selectedRow = depositTable.getSelectedRow();
+                    Object selectedPath = depositTable.getValueAt(selectedRow, 2);
+
+                    // add action listeners to the menu items
+                    copyItem.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            copyToClipboard(selectedPath.toString());
+                        }
+                    });
+                    openItem.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            try {
+                                Desktop desktop = Desktop.getDesktop();
+                                URI uri = new URI(selectedPath.toString());
+                                desktop.browse(uri);
+                            } catch (Exception ex) {
+                                ex.printStackTrace();
+                            }
+                        }
+                    });
+                    menu.add(copyItem);
+                    menu.add(openItem);
+                    menu.show(depositTable, e.getX(), e.getY());
+                }
+            }
+        });
     }
     private static void copyToClipboard(String content) {
         StringSelection stringSelection = new StringSelection(content);
